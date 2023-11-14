7667b092fc1fb6c84a79fd8d29005edef5e12e37
HeliaTutueanu <150536387+HeliaTutueanu@users.noreply.github.com>
2023-11-14 11:59:44 +0200

    added variable and readme file

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+alert('Hello world');
+
+// var age = 29;
+// var name = 'Anne';
+
+// let josesAge = 35;
+// let lisasAge = josesAge;
+
+let favoriteFood = 'Pizza';
+document.write(favoriteFood);
+favoriteFood = 'Butter Chicken';
+document.write(favoriteFood);
+
+// let size = 100;
+// let doubleSize = size * 2;
+// let minSize = (doubleSize * 2) - (size / 2);
+// document.write(minSize);
