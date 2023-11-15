50b0ff2234ad399d03fc7eb43e695f05462c1905
HeliaTutueanu <150536387+HeliaTutueanu@users.noreply.github.com>
2023-11-15 10:05:23 +0200

    added variable and readme file requirements

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,5 @@
-alert('Hello world');
+let pokemonList =
+   [{name: "Squirtle", height: "50cm", type: ["earth","cuteness"]},
+    {name: "Ivysaur", height: "30cm", type: ["water","scary"]},
+    {name: "Wigglytuff", height: "60cm", type: ["steel","poison"]}]
 
-// var age = 29;
-// var name = 'Anne';
-
-// let josesAge = 35;
-// let lisasAge = josesAge;
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
-favoriteFood = 'Butter Chicken';
-document.write(favoriteFood);
-
-// let size = 100;
-// let doubleSize = size * 2;
-// let minSize = (doubleSize * 2) - (size / 2);
-// document.write(minSize);
