2e4c8edbfedce5384d8931b6a6896be5dbabac2a
HeliaTutueanu <150536387+HeliaTutueanu@users.noreply.github.com>
2023-11-16 10:07:08 +0200

    updated css+html layout, added tota11y file, added js loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-let pokemonList =
-   [{name: "Squirtle", height: "50cm", type: ["earth","cuteness"]},
-    {name: "Ivysaur", height: "30cm", type: ["water","scary"]},
-    {name: "Wigglytuff", height: "60cm", type: ["steel","poison"]}]
+let pokemonList = [
+   {name: "Squirtle", height: 50, type: ["earth","cuteness"]},
+   {name: "Ivysaur", height: 30, type: ["water","scary"]},
+   {name: "Wigglytuff", height: 60, type: ["steel","poison"]}
+];
 
+for (let i = 0; i < pokemonList.length; i++) {
+   document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+   if (pokemonList[i].height > 55) {
+      document.write(" - Wow, that's big!");
+  }
+   else if (pokemonList[i].height > 30 && pokemonList[i].height <= 50) {
+   document.write(" - Amazing height!");
+  }
+   else {
+   document.write(" - You're so cute!");
+  }
+   document.write("<br>");
+}
