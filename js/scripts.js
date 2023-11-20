f1c2f2903a2fddde8d92e80b1b6e6fcd90fb46ba
HeliaTutueanu <150536387+HeliaTutueanu@users.noreply.github.com>
2023-11-20 10:03:37 +0200

    updated IIFE code and the foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,61 @@
-let pokemonList = [
-   {name: "Squirtle", height: 50, type: ["earth","cuteness"]},
-   {name: "Ivysaur", height: 30, type: ["water","scary"]},
-   {name: "Wigglytuff", height: 60, type: ["steel","poison"]}
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: "Squirtle", height: 50, type: ["earth","cuteness"]},
+    {name: "Ivysaur", height: 30, type: ["water","scary"]},
+    {name: "Wigglytuff", height: 60, type: ["steel","poison"]}
 ];
 
-let pokemonList2 = [
-   {name: "Alcremie", height: 50, type: ["fairy","magical"]},
-   {name: "Altaria", height: 70, type: ["dragon","flying"]},
-   {name: "Azurill", height: 25, type: ["water","magical"]}
-];
-
-function printArrayDetails(list){
-    list.forEach(function(pokemon) {
-    document.write(pokemon.name + " (height: " + pokemon.height + ")");
-    if (pokemon.height > 55) {
-      document.write(" - Wow, that's big!");
-  }
-    else if (pokemon.height > 30 && pokemon.height <= 50) {
-      document.write(" - Amazing height!");
-  }
-    else {
-      document.write(" - You're so cute!");
-  }
-      document.write("<br>");
-  });
+function getAll() {
+  return pokemonList;
 }
 
-printArrayDetails(pokemonList);
-printArrayDetails(pokemonList2);
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
+
+return {
+  getAll: getAll,
+  add: add
+};
+})();
+
+let pokemonRepository2 = (function () {
+  let pokemonList2 = [
+    {name: "Alcremie", height: 50, type: ["fairy","magical"]},
+    {name: "Altaria", height: 70, type: ["dragon","flying"]},
+    {name: "Azurill", height: 25, type: ["water","magical"]}
+];
+
+function getAll() {
+  return pokemonList2;
+}
+
+function add(pokemon) {
+  pokemonList2.push(pokemon);
+}
+
+return {
+  getAll: getAll,
+  add: add
+};
+})();
+
+let newPokemonRepository = pokemonRepository;
+let newPokemonRepository2 = pokemonRepository2;
+
+(pokemonRepository.getAll().concat(pokemonRepository2.getAll())).forEach(function(pokemon) {
+  document.write(pokemon.name + " (height: " + pokemon.height + ")");
+  if (pokemon.height > 55) {
+    document.write(" - Wow, that's big!");
+  }
+  else if (pokemon.height > 30 && pokemon.height <= 50) {
+    document.write(" - Amazing height!");
+  }
+  else {
+    document.write(" - You're so cute!");
+  }
+    document.write("<br>");
+  });
+
+printArrayDetails(newPokemonRepository.getAll());
+printArrayDetails(newPokemonRepository2.getAll());
