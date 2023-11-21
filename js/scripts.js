ff70a8dbadcedb52238b1f69b23afa3d80070edf
HeliaTutueanu <150536387+HeliaTutueanu@users.noreply.github.com>
2023-11-21 11:30:09 +0200

    added pokemon button, updated title + css

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,3 @@
-function mergedPokemonRepository(mergedList) {
-  let pokemonList = mergedList || [];
-  function getAll() {
-    return pokemonList;
-  }
-  function add(pokemon) {
-    const expectedKeys = ['name', 'height', 'type'];
-    if (typeof pokemon === 'object' && expectedKeys.every(key => Object.keys(pokemon).includes(key))) {
-      pokemonList.push(pokemon);
-    } else {
-      console.error('Invalid Pokemon format. Please provide an object with keys: ' + expectedKeys.join(', '));
-    }
-  }
-  function findByName(name) {
-    return pokemonList.find(pokemon => pokemon.name.toLowerCase() === name.toLowerCase());
-  }
-  return {
-    getAll: getAll,
-    add: add,
-    findByName: findByName
-  };
-}
-
 let pokemonRepository = mergedPokemonRepository([
   {name: "Squirtle", height: 50, type: ["earth","cuteness"]},
   {name: "Ivysaur", height: 30, type: ["water","scary"]},
@@ -33,22 +10,56 @@ let pokemonRepository2 = mergedPokemonRepository([
   {name: "Azurill", height: 25, type: ["water","magical"]}
 ]);
 
-let newPokemonRepository = pokemonRepository;
-let newPokemonRepository2 = pokemonRepository2;
+function mergedPokemonRepository(mergedList) {
+  let pokemonList = mergedList || [];
+  function getAll() {
+    return pokemonList;
+  }
+  function add(pokemon) {
+    const expectedKeys = ['name', 'height', 'type'];
+
+    if (typeof pokemon === 'object' && expectedKeys.every(key => Object.keys(pokemon).includes(key))) {
+      pokemonList.push(pokemon);
+    } else {
+      console.error('Invalid Pokemon format. Please provide an object with keys: ' + expectedKeys.join(', '));
+    }
+  }
+
+  function findByName(name) {
+    return pokemonList.find(pokemon => pokemon.name.toLowerCase() === name.toLowerCase());
+  }
+
+  function addListItem(pokemon) {
+    let ulElement = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+    listItem.appendChild(button);
+    ulElement.appendChild(listItem);
+
+    addClickListener(button, pokemon);
+  }
+
+  function showDetails(pokemon) {
+    console.log("Pokemon details:", pokemon);
+  }
+  
+  function addClickListener(button, pokemon) {
+    button.addEventListener('click', function () {
+      showDetails(pokemon);
+    });
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+    findByName: findByName,
+    addListItem: addListItem
+  };
+}
 
 (pokemonRepository.getAll().concat(pokemonRepository2.getAll())).forEach(function(pokemon) {
-  document.write(pokemon.name + " (height: " + pokemon.height + ")");
-  if (pokemon.height > 55) {
-    document.write(" - Wow, that's big!");
-  }
-  else if (pokemon.height > 30 && pokemon.height <= 50) {
-    document.write(" - Amazing height!");
-  }
-  else {
-    document.write(" - You're so cute!");
-  }
-    document.write("<br>");
-  });
-
-printArrayDetails(newPokemonRepository.getAll());
-printArrayDetails(newPokemonRepository2.getAll());
+  pokemonRepository.addListItem(pokemon);
+});
